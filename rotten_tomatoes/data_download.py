44f58645a3b6ffd0d2a55b49d905a71031c21b61
debsubdavis <95187410+debsubdavis@users.noreply.github.com>
2023-02-21 17:52:54 -0800

    Updated docstrings

--- rotten_tomatoes/data_download.py
+++ rotten_tomatoes/data_download.py
@@ -1,21 +1,37 @@
+"""
+Authenticates with the Kaggle API and downloads Kaggle datasets
 
+Notes:
+Pre-Requisite 1: kaggle installed in local environment. Installation instructions included here: https://www.kaggle.com/docs/api
+
+Pre-Requisite 2a: kaggle.json file created using the instructions included in the Authentication section of this website: https://www.kaggle.com/docs/api
+
+Pre-Requisite 2b: Save the kaggle.json file. Can be saved in the rotten_tomatoes folder, or anywhere on local machine. ().gitignore file includes rotten_tomatoes/kaggle.json)
+
+
+data_download exports the following functions: 
+    get_kaggle_creds
+    download_kaggle_datasets
+"""
 import os
 import json
 
 
-
-# Pre-Requisite 1: pip install kaggle
-
-
-# Pre-Requisite 2a: kaggle.json file created using the instructions 
-# included in the Authentication section of this website:
-# https://www.kaggle.com/docs/api
-
-
-# Pre-Requisite 2b: Save the kaggle.json file in the src folder
-# .gitignore file includes rotten_tomatoes/kaggle.json
-
 def get_kaggle_creds(kaggle_json_file_loc):
+    """Returns a username and password (key) from kaggle.json file
+
+    Parameters
+    ----------
+    kaggle_json_file_loc : string
+        File location for where the kaggle.json file is saved
+        
+    Returns
+    -------
+    username : string
+        Kaggle username
+    password : string
+        Kaggle password (key)
+    """
     #print("in get_kaggle_creds")
 
     with open(kaggle_json_file_loc, "r") as f:
@@ -29,12 +45,30 @@ def get_kaggle_creds(kaggle_json_file_loc):
 
 
 
-def download_kaggle_datasets(username, password, kaggle_dataset_list, file_output): 
+def download_kaggle_datasets(username, password, kaggle_dataset_list, file_output):
+    """Downloads all files from kaggle_dataset_list to the file_output location
+
+    Parameters
+    ----------
+    username : string
+        Kaggle username
+    password : string
+        Kaggle password (key)
+    kaggle_dataset_list : list
+        List of kaggle dataset to download. Each string in the list needs to be in "username/dataset" format
+    file_output : string
+        String for location where downloaded files should be saved. Recommend saving in a data/raw directory
+        
+    """ 
     #print("in download_kaggle_dataset\n")
 
     os.environ['KAGGLE_USERNAME'] = username
-    os.environ['KAGGLE_KEY'] = password  
+    os.environ['KAGGLE_KEY'] = password
 
+    # DO NOT MOVE THIS IMPORT STATEMENT TO THE TOP OF THE FILE
+    # When these import statements run, they look for kaggle.json in locations listed in the Kaggle documentation https://www.kaggle.com/docs/api
+    # Reference from Kaggle documentation: "the tool will look for this token at ~/.kaggle/kaggle.json on Linux, OSX, and other UNIX-based operating systems, and at C:\Users<Windows-username>.kaggle\kaggle.json on Windows"
+    # DO NOT MOVE THIS IMPORT STATEMENT TO THE TOP OF THE FILE
     from kaggle.api.kaggle_api_extended import KaggleApi
     import kaggle
 
