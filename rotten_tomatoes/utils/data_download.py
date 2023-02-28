64e4dde9cecff7180cd89e606227d377b7a210d3
Emily Linebarger <elineb@uw.edu>
2023-02-28 07:55:45 -0800

    Progress on pylint

--- rotten_tomatoes/utils/data_download.py
+++ rotten_tomatoes/utils/data_download.py
@@ -2,11 +2,14 @@
 Authenticates with the Kaggle API and downloads Kaggle datasets
 
 Notes:
-Pre-Requisite 1: kaggle installed in local environment. Installation instructions included here: https://www.kaggle.com/docs/api
+Pre-Requisite 1: kaggle installed in local environment. 
+Installation instructions included here: https://www.kaggle.com/docs/api
 
-Pre-Requisite 2a: kaggle.json file created using the instructions included in the Authentication section of this website: https://www.kaggle.com/docs/api
+Pre-Requisite 2a: kaggle.json file created using the instructions included 
+in the Authentication section of this website: https://www.kaggle.com/docs/api
 
-Pre-Requisite 2b: Save the kaggle.json file. Can be saved in the rotten_tomatoes folder, or anywhere on local machine. ().gitignore file includes rotten_tomatoes/kaggle.json)
+Pre-Requisite 2b: Save the kaggle.json file. Can be saved in the rotten_tomatoes folder, 
+or anywhere on local machine. ().gitignore file includes rotten_tomatoes/kaggle.json)
 
 
 data_download exports the following functions: 
@@ -56,13 +59,17 @@ def download_kaggle_datasets(username, password, kaggle_dataset_list, file_outpu
     password : string
         Kaggle password (key)
     kaggle_dataset_list : list
-        List of kaggle dataset to download. Each string in the list needs to be in "username/dataset" format
+        List of kaggle dataset to download. 
+        Each string in the list needs to be in "username/dataset" format
     file_output : string
-        String for location where downloaded files should be saved. Recommend saving in a data/raw directory
+        String for location where downloaded files should be saved. 
+        Recommend saving in a data/raw directory
 
     ValueError
-        - Any of the strings in the kaggle_dataset_list are not in the username/dataset format
-        - Any of the strings in the kaggle_dataset_list return 403 error from the kaggle.api.dataset_download_files api call
+        - Any of the strings in the kaggle_dataset_list 
+            are not in the username/dataset format
+        - Any of the strings in the kaggle_dataset_list 
+            return 403 error from the kaggle.api.dataset_download_files api call
 
 
     """
@@ -70,8 +77,11 @@ def download_kaggle_datasets(username, password, kaggle_dataset_list, file_outpu
     os.environ["KAGGLE_KEY"] = password
 
     # DO NOT MOVE THIS IMPORT STATEMENT TO THE TOP OF THE FILE
-    # When these import statements run, they look for kaggle.json in locations listed in the Kaggle documentation https://www.kaggle.com/docs/api
-    # Reference from Kaggle documentation: "the tool will look for this token at ~/.kaggle/kaggle.json on Linux, OSX, and other UNIX-based operating systems, and at C:\Users<Windows-username>.kaggle\kaggle.json on Windows"
+    # When these import statements run, they look for kaggle.json 
+    # in locations listed in the Kaggle documentation https://www.kaggle.com/docs/api
+    # Reference from Kaggle documentation: "the tool will look for this 
+    # token at ~/.kaggle/kaggle.json on Linux, OSX, and other UNIX-based operating systems, 
+    # and at C:\Users<Windows-username>.kaggle\kaggle.json on Windows"
     # DO NOT MOVE THIS IMPORT STATEMENT TO THE TOP OF THE FILE
     from kaggle.api.kaggle_api_extended import KaggleApi
     import kaggle
